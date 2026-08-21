2d7fb12992fa4ab7d1071953db763e91c101c06e
paparul29 <resizu2010@gmail.com>
2026-08-20 20:28:55 -0600

    users corrected, ya tiene api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
