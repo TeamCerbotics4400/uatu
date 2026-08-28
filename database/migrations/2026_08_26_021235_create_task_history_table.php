19a9721d9f7620862c2a68575083877a41d98f12
Santiago Mireles <santi26011830@gamil.com>
2026-08-27 20:30:28 -0600

    Task history migration created

--- database/migrations/2026_08_26_021235_create_task_history_table.php
+++ database/migrations/2026_08_26_021235_create_task_history_table.php
@@ -13,11 +13,19 @@ return new class extends Migration
     {
         Schema::create('task_history', function (Blueprint $table) {
             $table->id();
-            $table->foreign('service_task_id')->constrained('service_task')->cascadeOnDelete();
-            $table->foreign('user_id')->nullable()->constrained('users');
             $table->string('previous_state')->nullable();
             $table->string('new_state');
+            //primero lo declaras  para q no te error
+            $table->unsignedBigInteger('service_task_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            
             $table->timestamps();
+
+
+//van desoues del timestamps 
+
+$table->foreign('service_task_id')->references('id')->on('service_task');
+$table->foreign('user_id')->references('id')->on('users');
         });
     }
 
